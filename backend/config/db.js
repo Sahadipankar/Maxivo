825de2ba20b477d338c38724760f0cb48300155b
Sahadipankar <sahadepankar@gmail.com>
2026-07-18 10:27:25 +0530

    database connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("DB connected successfully")
+        await mongoose.connect(process.env.MONGODB_URL, {
+            dbName: 'Maxivo'
+        });
+        console.log("DB connected successfully");
     } catch (error) {
         console.log("DB connection error:", error)
     }
